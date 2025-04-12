d382d3d0591436e59c5dd0e48ca8e03f2f93d347
Vũ Đức Trung <vuductrung14112001@gmail.com>
2025-04-12 12:45:49 +0700

    gen qr code

--- public/index.js
+++ public/index.js
@@ -3,5 +3,6 @@ const output = document.getElementById('qr-output');
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log("submitted");
+    const link = document.getElementById('link-input').value;
+    output.innerHTML = `<img src="/generate?link-input=${link}" alt="QR Code">`;
 });
--- server.js
+++ server.js
@@ -1,47 +1,57 @@
 import http from 'node:http';
 import * as fs from 'fs';
+import qr from 'qr-image';
+import url from 'node:url';
 
+const isValidURL = (link) => {
+    const regex = /^(https?:\/\/)?(www\.)?([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}([\/\w\-.?=&%]*)*\/?$/;
+    return regex.test(link);
+};
 
 const server = http.createServer((req, res) => {
-    const url = req.url;
+    const parsedUrl = url.parse(req.url, true);
+    const pathname = parsedUrl.pathname;
+    const query = parsedUrl.query;
 
-     // 📝 Explanation:
-    // 'req.url' gives you the path part of the URL that the user requested.
-    // For example:
-    // - If user visits 'http://localhost:8000/', then url === '/'
-    // - If user visits 'http://localhost:8000/about', then url === '/about'
-    // - If the HTML page includes <link rel="stylesheet" href="/style.css">,
-    //   the browser will request '/style.css', so url === '/style.css'
-    // You can use this to determine which file to serve.
-
-    if (url === '/') {
+    if (pathname === '/') {
         fs.readFile('./public/index.html', function(err, data) {
             if (err) throw err;
 
             res.writeHead(200, {'Content-Type': 'text/html'});
             res.end(data);
         });
-    } else if (url === '/about') {
+    } else if (pathname === '/about') {
         fs.readFile('./public/about.html', function(err, data) {
             if (err) throw err;
 
             res.writeHead(200, {'Content-Type': 'text/html'});
             res.end(data);
         });
-    } else if (url === '/style.css') {
+    } else if (pathname === '/style.css') {
         fs.readFile('./public/style.css', function(err, data) {
             if (err) throw err;
 
             res.writeHead(200, {'Content-Type': 'text/css'});
             res.end(data);
         });
-    } else if (url === '/index.js') {
+    } else if (pathname === '/index.js') {
         fs.readFile('./public/index.js', function(err, data) {
             if (err) throw err;
     
             res.writeHead(200, {'Content-Type': 'application/javascript'});
             res.end(data);
         });
+    } else if (pathname === '/generate') {
+        const link = query['link-input'];
+
+        if (isValidURL(link)){
+            res.writeHead(200, {'Content-Type': 'image/png'});
+            let qr_png = qr.image(link, {type : 'png'});
+            qr_png.pipe(res);
+        } else {
+            res.writeHead(200);
+            res.end('Invalid link');
+        }
     }
     else {
         res.writeHead(400);
